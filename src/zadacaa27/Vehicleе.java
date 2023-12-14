5aedc02187a2faffdf3f8e126a3a6b763cfecc8c
MitkovskaM <mihaela.mitkovska@gmail.com>
2023-12-14 18:02:01 +0100

    Add first java proekt

---
+++ src/zadacaa27/Vehicleе.java
@@ -0,0 +1,16 @@
+package zadacaa27;
+
+public class Vehicleе {
+	  private String color;
+
+	    Vehicleе(String a) {
+	        color = a;
+	    }
+	    Vehicleе(){
+	    	color="Blue";
+	    }
+	    public String toString() {
+	        return color;
+	    }
+	}
+
---
+++ src/zadacaa27/myClass.java
@@ -0,0 +1,13 @@
+package zadacaa27;
+
+public class myClass {
+
+	public static void main(String[] args) {
+			Vehicleе v1=new Vehicleе("Red");
+			Vehicleе v2=new Vehicleе ();
+			System.out.println(v1);
+			System.out.println(v2);
+			
+	}
+
+}
